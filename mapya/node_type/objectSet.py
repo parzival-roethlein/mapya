7009f1e5bd1b5bdbf3dc2b33a3b9392e5d37871f
Parzival Roethlein <pa.roethlein@gmail.com>
2018-08-19 19:11:14 +0200

    update docstring, added attr placeholders

---
+++ mapya/node_type/objectSet.py
@@ -0,0 +1,33 @@
+
+
+from .. import utils
+from .dependNode import DependNode
+
+
+class ObjectSet(DependNode):
+    @utils.SetterProperty
+    def dnMembers(self, value):
+        pass
+
+    @utils.SetterProperty
+    def dagMembers(self, value):
+        pass
+
+    '''
+    def __len__(self):
+        return len(self.members)
+        #return mc.sets(self.name, q=True, size=True)
+    def add(self):
+        pass
+    def remove(self):
+        pass
+    def remove_try(self):
+        # remove object, but without error if non-member? look up naming convention
+        pass
+    def addMaya(self):
+        'behaves like maya.cmds.sets: only adds an object once'
+        pass
+    def removeMaya(self):
+        'behaves like maya.cmds.sets: no error when removing non-member'
+        pass
+   '''
