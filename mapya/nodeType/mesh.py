9dd1c0b866afe7f1ddf34f8c6a2cbe99512ea4a2
Parzival Roethlein <pa.roethlein@gmail.com>
2020-02-02 17:10:24 -0500

    points getter and setter method added for space arg

--- mapya/nodeType/mesh.py
+++ mapya/nodeType/mesh.py
@@ -19,15 +19,19 @@ from mapya.plugin.prSetPntsCmd import py_prSetPntsCmd
 
 class Mesh(DeformableShape):
 
-    # TODO: needs setter function to give optional point space. maybe replace with .vtx Attribute()?!
     @property
     def points(self):
-        return om.MFnMesh(self.MDagPath).getPoints()
+        return self.getPoints()
 
     @points.setter
     def points(self, value):
+        self.setPoints(value)
+
+    def getPoints(self, space=om.MSpace.kObject):
+        return om.MFnMesh(self.MDagPath).getPoints(space)
+
+    def setPoints(self, value, space=om.MSpace.kObject):
         if not mc.pluginInfo('prSetPntsCmd', q=True, loaded=True):
             from mapya.plugin import prSetPntsCmd
             mc.loadPlugin(prSetPntsCmd.__file__.replace('.pyc', '.py'))
-        py_prSetPntsCmd(self.name, positions=value)
-
+        py_prSetPntsCmd(self.name, value, space)
